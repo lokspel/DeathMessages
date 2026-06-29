361495f712c5e3ac4d54918fc3f68f9794f4b4b2
lokspel <208148594+lokspel@users.noreply.github.com>
2026-06-29 10:31:28 +0400

    cleanup

--- src/main/java/me/lokspel/deathmessages/utils/MessageUtils.java
+++ src/main/java/me/lokspel/deathmessages/utils/MessageUtils.java
@@ -3,19 +3,8 @@ package me.lokspel.deathmessages.utils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 
 public class MessageUtils {
-
-    private static final PlainTextComponentSerializer plainSerializer = PlainTextComponentSerializer.plainText();
-
-    /**
-     * Extracts plain text from a Component, removing all formatting.
-     */
-    public static String getPlainText(Component component) {
-        return plainSerializer.serialize(component);
-    }
 
     /**
      * Colors a name with the given color and removes hover events.
@@ -24,12 +13,4 @@ public class MessageUtils {
         return Component.text(name).color(color).hoverEvent(null);
     }
 
-    /**
-     * Sends a message to all online players.
-     */
-    public static void broadcastMessage(Component message) {
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            player.sendMessage(message);
-        }
-    }
 }
